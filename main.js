b374d4108091f262e9b6bd451e9f15eb5db5631d
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 17:29:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function make_shirt(size, lable) {
+    return 'my shirt size is ${size}and my lable is ${lable}';
+}
+var myFun = make_shirt(30, "The sunny day");
+console.log(myFun);
---
+++ main.ts
@@ -0,0 +1,7 @@
+function make_shirt(size:number,lable:string){
+   return 'my shirt size is ${size}and my lable is ${lable}'
+
+}
+ let myFun = make_shirt(30,"The sunny day")
+ console.log(myFun);
+   
